d1e3841c63b1132d713837b828d91ee02f07ffa9
anujith <anujith.c@gmail.com>
2022-06-14 15:41:27 +0530

    added relevent models for auth app

--- apps/social_auth/models.py
+++ apps/social_auth/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser as DjangoUser
 
-# Create your models here.
+
+class SocialUser(DjangoUser):
+    """
+    The Social User inherited from Django User
+    """
+    nick_name = models.CharField(max_length=24, null=True, blank=True)
+
+    @property
+    def profile_name(self):
+        return str(self)
+
+    def __str__(self):
+        if self.nick_name:
+        	return "{} {} ({})".format(
+        		self.first_name, self.last_name, self.nick_name)
+        else:
+            return "{} {}".format(
+        		self.first_name, self.last_name)
