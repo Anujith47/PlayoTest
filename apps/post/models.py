0612e6c31b653ce8b9cd38432b2eef1b1f293cd0
anujith <anujith.c@gmail.com>
2022-06-14 15:40:29 +0530

    added relevent models for post app

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,3 +1,37 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+from apps.social_auth.models import SocialUser
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=100)
+    user = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
+    pub_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["-pub_time"]
+
+    def __str__(self):
+        return self.title
+
+
+class Like(models.Model):
+    user = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    liked_time = models.DateField(default=timezone.now)
+
+    class Meta:
+        ordering = ["-liked_time"]
+        unique_together = ('user', 'post',)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commented_time = models.DateField(default=timezone.now)
+    comment_description = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["-commented_time"]
